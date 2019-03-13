1857c0f3e7c85761d873adf49dae17d3274dc3c7
Alexander Petrenko <j.alex.ptr@gmail.com>
2019-03-13 18:09:06 +0300

    Save to csv

--- js/content.js
+++ js/content.js
@@ -16,7 +16,7 @@ function updateControls() {
     console.log('Yandex Direct Plugin: update controls');
 
     $('tr.js-phrase-row-template').each((index, row) => {
-        let phrase = $(row).find('td:nth-child(3) span').first().text();
+        let phrase = $(row).find('td:nth-child(3) span').first().text().trim();
         let list   = $(row).find('td:nth-child(4) ul').first();
 
         if (list.children().length <= 3) {
@@ -40,25 +40,31 @@ function updateControls() {
 
 function collectContacts(phrase, cb) {
     let geo = $('#geo').val();
+    let geoNames = $('.b-region-selector__geo-text-fact').text().trim();
+    let req = {
+        phrase: phrase,
+        geo: geo,
+        geoNames: geoNames
+    };
 
     $.ajax({
         url: `https://direct.yandex.ru/registered/main.pl?cmd=showCompetitors&phrase=${phrase}&geo=${geo}`,
         success: (data) => {
-            parseAds(data, cb);
+            parseAds(req, data, cb);
         }
     });
     console.log('Collect contants: phrase = ' + phrase + '; geo = ' + geo);
 }
 
 
-function parseAds(html, cb) {
+function parseAds(req, html, cb) {
     let result   = [];
     let spans    = $(html).find('.b-banner-preview__footer > span.link');
     let count    = spans.length;
     let onResult = () => {
         count = count - 1;
         if (count <= 0) {
-            saveToFile(result);
+            saveToFile(req, result);
             cb();
         }
     };
@@ -79,19 +85,32 @@ function parseAds(html, cb) {
 
 
 function parseContacts(html) {
-    let name  = $(html).find('.p-view-vcard-headless__vcard-name').text();
-    let phone = $(html).find('.p-view-vcard-headless__phone .p-view-vcard-headless__info').text();
-    let site  = $(html).find('.b-banner-preview__domain').text();
+    let name  = $(html).find('.p-view-vcard-headless__vcard-name').text().trim();
+    let phone = $(html).find('.p-view-vcard-headless__phone .p-view-vcard-headless__info').text().trim();
+    let site  = $(html).find('.b-banner-preview__domain').text().trim();
 
     return {
-        name: name,
+        name: name.replace(/"/g, "'"),
         phone: phone,
         site: site
     }
 }
 
 
-function saveToFile(data) {
-    console.log(data);
+function saveToFile(req, data) {
+    let csvHeader   = `"Поиск по фразе:";"${req.phrase}"\n"Регионы:";"${req.geoNames}"`;
+    let columnNames = `"Название компании";"Телефон";"Сайт"`;
 
+    let csv = data.reduce(
+        (result, obj) => `${result}\n"${obj.name}";"${obj.phone}";"${obj.site}"`,
+        `${csvHeader}\n\n${columnNames}`);
+
+    let contentType = 'text/csv';
+    let blob        = new Blob(['\ufeff' + csv], { type: contentType });
+    let anchor      = document.createElement('a');
+
+    anchor.download            = `${req.phrase}.csv`;
+    anchor.href                = (window.webkitURL || window.URL).createObjectURL(blob);
+    anchor.dataset.downloadurl = [contentType, anchor.download, anchor.href].join(':');
+    anchor.click();
 }
