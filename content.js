1b8bedded4858333cdfa39cdaac70895fa02da54
Alexander Petrenko <j.alex.ptr@gmail.com>
2019-03-13 03:57:23 +0300

    Add data parsing

--- content.js
+++ content.js
@@ -1,42 +1,88 @@
 console.log('Yandex Direct Plugin: start');
+initListeners();
 
-if (jQuery) {
-    console.log('Yandex Direct Plugin: jQuery no conflict');
-    jQuery.noConflict();
-}
 
-jQuery(document).ready(() => {
-    jQuery('.b-advanced-forecast__result-table__table').on('DOMSubtreeModified', function() {
+function initListeners() {
+    console.log('Yandex Direct Plugin: init listeners');
+
+    let table = $('.b-advanced-forecast__result-table__table');
+    table.on('DOMSubtreeModified', function() {
         updateControls();
     });
-    jQuery(document).on('click', '.js-collect-contacts', () => {
-        let geo    = jQuery('#geo').val();
-        let phrase = jQuery(this).attr('title');
-
-        collectContacts(phrase, geo);
-    });
-
-    updateControls();
-});
+}
 
 
 function updateControls() {
-    console.log('Yandex Direct Plugin: updateControls');
+    console.log('Yandex Direct Plugin: update controls');
 
-    jQuery('tr.js-phrase-row-template').each((index, row) => {
-        let phrase = jQuery(row).find('td:nth-child(3) span').first().text();
-        let list   = jQuery(row).find('td:nth-child(4) ul').first();
+    $('tr.js-phrase-row-template').each((index, row) => {
+        let phrase = $(row).find('td:nth-child(3) span').first().text();
+        let list   = $(row).find('td:nth-child(4) ul').first();
 
         if (list.children().length <= 3) {
-            console.log('Yandex Direct Plugin: link added');
-            list.append(`<li><span class="js-collect-contacts b-pseudo-link" title="${phrase}">собрать контакты</span></li>`);
+            let li   = $(`<li></li>`);
+            let span = $(`<span class="b-pseudo-link">собрать контакты</span>`);
+            span.click(() => {
+                let geo = $('#geo').val();
+                collectContacts(phrase, geo);
+            });
+
+            li.append(span);
+            list.append(li);
         }
     });
 }
 
 
 function collectContacts(phrase, geo) {
-    console.log('Collect contants using phrase: '+ phrase);
+    console.log('Collect contants: phrase = ' + phrase + '; geo = ' + geo);
 
-    //ajax get html
+    $.ajax({
+        url: `https://direct.yandex.ru/registered/main.pl?cmd=showCompetitors&phrase=${phrase}&geo=${geo}`,
+        success: (data) => {
+            parseAds(data);
+        }
+    });
+}
+
+
+function parseAds(html) {
+    let result = [];
+    let spans = $(html).find('.b-banner-preview__footer > span.link');
+    let count = spans.length;
+
+    spans.each((index, row) => {
+        let bem = $(row).data('bem');
+        let url = bem['b-modal-popup-opener']['url'];
+
+        $.ajax({
+           url: url,
+           success: (data) => {
+               result[index] = parseContacts(data);
+               count = count - 1;
+               if (count <= 0) {
+                   saveToFile(result);
+               }
+           }
+        });
+    });
+}
+
+
+function parseContacts(html) {
+    let name  = $(html).find('.p-view-vcard-headless__vcard-name').text();
+    let phone = $(html).find('.p-view-vcard-headless__phone .p-view-vcard-headless__info').text();
+    let site  = $(html).find('.b-banner-preview__domain').text();
+
+    return {
+        name: name,
+        phone: phone,
+        site: site
+    }
+}
+
+
+function saveToFile(data) {
+    console.log(data);
+
 }
